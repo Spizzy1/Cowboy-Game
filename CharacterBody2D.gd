a959ed599ccf9e59a8eda3fa588ae79d706c19a0
Spizzy <spizzy47spizzy@gmail.com>
2024-03-04 16:04:14 +0100

    initial commit

---
+++ CharacterBody2D.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+	velocity = direction * SPEED
+
+	move_and_slide()
