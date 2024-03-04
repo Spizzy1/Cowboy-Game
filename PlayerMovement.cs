69bdd3ec52ec43d0463add03eabd89f44d368b50
baptul-bim <baptul.bim@gmail.com>
2024-03-04 16:51:30 +0100

    wow defaul code

---
+++ PlayerMovement.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class PlayerMovement : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+
+
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+
+		velocity = direction * Speed;
+
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
