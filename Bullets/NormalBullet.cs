8c3b448b55da75beb717242966d9ed31bd13a0ca
baptul-bim <baptul.bim@gmail.com>
2024-03-05 15:45:56 +0100

    *ko som tuggar gif*

--- Bullets/NormalBullet.cs
+++ Bullets/NormalBullet.cs
@@ -6,11 +6,12 @@ public partial class NormalBullet : BulletBase
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		bulletSpd = 100;
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 	}
 }
--- PlayerShooting.cs
+++ PlayerShooting.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class PlayerShooting : Node
 {
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -14,17 +16,21 @@ public partial class PlayerShooting : Node
 		
 		if(@event.IsActionReleased("action_1")){
 			
-
-			GD.Print(GetParent().GetNode<Inventory>("Inventory").get_current_cylinder());
-
-			//snälla framtida louie, 
+			var scene = GD.Load<PackedScene>("res://Bullets/bullet_real.tscn");
+			GD.Print(GetParent().GetNode<Inventory>("InventorySystem").get_current_cylinder());
+			var instance = scene.Instantiate<Node2D>();
+			
+			GetParent().GetParent().AddChild(instance);
+			instance.Position = GetOwner<Node2D>().GlobalPosition;
+			
+			//snälla framtida louie, ö
 			//fixa dena fula (men roliga) kod. jag lämnar detta ärade uppdrag till dig för att jag (nutida louie) orkar inte just nu. 
 			//tack :))
-			if(GetParent().GetNode<Inventory>("Inventory").get_current_cylinder() != null){
+			if(GetParent().GetNode<Inventory>("InventorySystem").get_current_cylinder() != null){
 				GD.Print("pew pew you shoot wow hahahahahah ahahahahha wowza!!!! yippeeeE!!!! yippe yahooooo!!!! wahooooo!!!!!");
 
-
-				GetParent().GetNode<Inventory>("Inventory").change_cylinder();
+				
+				GetParent().GetNode<Inventory>("InventorySystem").change_cylinder();
 			}
 			else{
 				GD.Print("not work (good)");
