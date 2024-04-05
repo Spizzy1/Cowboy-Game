05bb8b9d7d3c439f1d2654c0bf69e23317605d6f
baptul-bim <baptul.bim@gmail.com>
2024-04-05 18:01:51 +0200

    State Machine start

---
+++ Enemies/Standoff.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class Standoff : State
+{
+    public override void Enter(){
+        GD.Print("i am standoff now (crazy how that works)");
+    }
+
+
+    
+}
